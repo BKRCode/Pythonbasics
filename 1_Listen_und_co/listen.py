273922cd93feec2b8412476bd1244bf1153f0df5
Lukas Becker <Lukas-becker@outlook.com>
2024-10-17 10:50:57 +0200

    Listen und co

---
+++ 1_Listen_und_co/listen.py
@@ -0,0 +1,35 @@
+#Listen funktionieren wie die ArrayList in Java
+
+liste = ['Äpfel', 'Birnen', 'Tomaten']
+
+print (liste)
+
+print (liste[1]) # Birnen
+
+
+print('For-Each-Construct')
+# for-each
+for i in liste:
+    print(i)
+
+
+# Auch mit Zahlen
+
+liste = [12, 38, 48, 2]
+print('Liste mit Zahlenwerten')
+for i in liste:
+    print(i)
+
+
+# Addition der ausgegebenen werte
+
+# Liste mit den Werten
+zahlen = [12, 45, 89, 4]
+
+
+summe = 0
+for zahl in zahlen:
+    summe += zahl
+print("Hier der Addierte Zahlenwert:", summe)
+
+    
