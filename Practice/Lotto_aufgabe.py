7cde0af11af8f155375ac8e7427a8e1bd9e1061a
Lukas Becker <Lukas-becker@outlook.com>
2024-10-18 11:02:27 +0200

    Cleanup
    Removed for clarity

--- Practice/Lotto_aufgabe.py
+++ Practice/Lotto_aufgabe.py
@@ -1,18 +1,3 @@
-"""
-AUFGABE:
- 
-Schreibe ein Programm, mit dem Du Lotto spielen kannst. 6 aus 49.
- 
-Als Eingabe müssen 6 unterschiedliche Zahlen zwischen 1 und 49 aufgenommen werden. Als siebte Zahl erfolgt dann die Angabe der Superzahl (0 - 9)
- 
-In einer danach folgenden Schleife sollen per Zufallsgenerator ebenso 6 unterschiedliche(!) Zahlen gezogen und eine Superzahl ermittelt werden. Eine count Variable soll die Anzahl der insgesamten Durchläufe anzeigen.
- 
-Jede Gewinnkombination soll auf der Konsole ausgegeben werden.
- 
-Die Schleife ist fertig, wenn 6 Richtige inkl. Superzahl gezogen wurden. Dann die Gewinnkombination ausgeben und die Anzahl der Ziehungen.
-
-Bonus aufgabe: Ende des Durchlaufes soll auch ausgegeben werden, wie oft welche Gewinncombo gezogen wurde
-"""
 import random
 
 def lotto_spiel():
